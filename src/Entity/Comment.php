5565b46661500450a19b26bae6be2180cac55d1d
Morgane Mahé <maeamahe@gmail.com>
2024-05-02 15:56:01 +0200

    add entitities and repositories

---
+++ src/Entity/Comment.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Comment\Repository\CommentRepository;
+use App\Repository\CommentRepository as RepositoryCommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RepositoryCommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $published_at = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    private Article $article;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->published_at;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $published_at): static
+    {
+        $this->published_at = $published_at;
+
+        return $this;
+    }
+
+    public function getArticle(): Article
+    {
+        return $this->article;
+    }
+
+    public function setArticle(Article $article): static
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
